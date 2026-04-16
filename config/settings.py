b1a320dd06d2b210b2a793ab60720b050e1e6b79
NadezhdaZakharova <nadika.zakharova@gmail.com>
2026-04-16 21:05:44 +0300

    Prod static: STATIC_ROOT, collectstatic, volume for nginx

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
